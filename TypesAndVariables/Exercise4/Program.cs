f15df2db5eeb139244a2dfa7e59775d4489d8d9d
Ana Dika <ana.jekimova@gmail.com>
2021-07-14 15:52:09 +0300

    Exsises 4

---
+++ TypesAndVariables/Exercise4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int birthYear;
+            Console.WriteLine("Enter name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter year of birth");
+             int.TryParse(Console.ReadLine(), out birthYear);
+
+            Console.WriteLine($"My name is {name} and was born in {birthYear}");
+
+
+        }
+    }
+}
